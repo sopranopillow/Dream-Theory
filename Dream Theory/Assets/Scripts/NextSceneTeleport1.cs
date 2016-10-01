ca609c7c5d3fc6dca57d2ca4f0c8ba2e2ed248fb
DavidDSS <daviddss1999@hotmail.com>
2016-10-01 00:31:44 -0500

    Change #11
    - Added Script to teleport to next Scene
    - Added box colliders to map
    - Minor changes

---
+++ Dream Theory/Assets/Scripts/NextSceneTeleport1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class NextSceneTeleport1 : MonoBehaviour {
+
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerTransform.position.y<19)
+        {
+            EditorSceneManager.LoadScene(2);
+        }
+
+        Debug.Log("X= " + playerTransform.position.x);
+        Debug.Log("Y= " + playerTransform.position.y);
+        Debug.Log("Z= " + playerTransform.position.z);
+
+    }
+}
