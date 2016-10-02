8271645e5a7e2852ac55b3a4fa76f266b9040acb
sopranopillow <esteban.230@hotmail.com>
2016-10-01 21:24:35 -0500

    Change #14
    -New features

---
+++ Dream Theory/Assets/Scripts/FanRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanRotation : MonoBehaviour {
+
+	public Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.FindGameObjectWithTag("Rotater").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerTransform.Rotate(0, 0.2f, 0);
+	}
+}
