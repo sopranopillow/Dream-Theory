5847b755e1c7f6191a39d8788f836e1b327d78c4
DavidDSS <daviddss1999@hotmail.com>
2016-10-01 23:31:39 -0500

    Change #15
    - Added moving books
    - Minor fixes

---
+++ Dream Theory/Assets/Scripts/BookFwd.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BookFwd : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+
+    public Transform playerTransform;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        playerTransform.Rotate(.5f, .5f, .5f);
+
+        if (playerTransform.position.x>244)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
