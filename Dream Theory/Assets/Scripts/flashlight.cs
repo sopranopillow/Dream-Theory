1a2e1062700ec33b64229e6edf3d2498eb7492e2
bernovie <bearnyoviedo@gmail.com>
2016-10-03 09:40:53 -0500

    Adds Flashlight
    A flashlight script and menu music were added

---
+++ Dream Theory/Assets/Scripts/flashlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flashlight : MonoBehaviour {
+
+	public Light flashlight2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F) && flashlight2.enabled == false){
+			flashlight2.enabled = true;
+		}else if(Input.GetKeyDown(KeyCode.F) && flashlight2.enabled == true){
+			flashlight2.enabled = false;
+		}
+	}
+
+}
