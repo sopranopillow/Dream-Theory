bce19bf96568dbfbacf76b4f0529ad6e636e35e5
bernovie <bearnyoviedo@gmail.com>
2016-10-03 10:13:31 -0500

    Adds lighter capabilities
    A spotlight that looks like fire

--- Dream Theory/Assets/Scripts/sound_oneshot.cs
+++ Dream Theory/Assets/Scripts/sound_oneshot.cs
@@ -4,17 +4,25 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 
 public class sound_oneshot : MonoBehaviour {
-	public AudioClip impact;
+	public AudioClip on;
+	public AudioClip off;
 	AudioSource audio;
 	public KeyCode key;
+	bool enabled;
 	void Start() {
 		audio = GetComponent<AudioSource>();
 		key = GetComponent<KeyCode>();
+		enabled = false;
 	}
 
 	void Update() {
-		if(Input.GetKeyDown(KeyCode.F)){
-			audio.PlayOneShot(impact, 0.7F);
-		}	
+		if(Input.GetKeyDown(KeyCode.F) && enabled == false){
+			audio.PlayOneShot(on, 0.7F);
+			enabled = !enabled;
+		}else if(Input.GetKeyDown(KeyCode.F) && enabled == true){
+			audio.PlayOneShot(off, 0.7F);
+			enabled = !enabled;
+		}
+
 	}
 }
