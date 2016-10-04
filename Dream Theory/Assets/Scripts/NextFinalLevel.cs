de1927777e8fdf96be0d119901a056f0b872b33a
sopranopillow <esteban.230@hotmail.com>
2016-10-04 13:17:02 -0500

    Change #29
    - Finished totem
    - Finished level 3 (needs notes)

---
+++ Dream Theory/Assets/Scripts/NextFinalLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class NextFinalLevel : MonoBehaviour {
+
+	private Transform playerTransform;
+
+	public float fallArea;
+
+	public int sceneNum;
+
+	public GameObject gameObj;
+
+	// Use this for initialization
+	void Start () {
+
+		playerTransform = gameObj.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playerTransform.position.y<fallArea)
+		{
+			EditorSceneManager.LoadScene(sceneNum);
+		}
+
+		Debug.Log("X= " + playerTransform.position.x);
+		Debug.Log("Y= " + playerTransform.position.y);
+		Debug.Log("Z= " + playerTransform.position.z);
+
+	}
+}
---
+++ Dream Theory/Assets/Scripts/TotemActivator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TotemActivator : MonoBehaviour {
+
+	public GameObject obj;
+
+	public float timer= 0.0f; // begins at this value
+	public float timerMax = 5.0f; // event occurs at this value
+
+	public bool act=false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			act = true;
+		}
+
+		if (act == true) {
+			obj.SetActive (true);
+
+			timer += Time.deltaTime;
+
+
+			if (timer >= timerMax)
+			{
+				Debug.Log(timer);
+				#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+				#else
+				Application.Quit();
+				#endif 
+
+			}
+		}
+	}
+}
