e0d95ff998a0cfa0dc466cccad8bf81253a6253b
sopranopillow <esteban.230@hotmail.com>
2016-10-03 22:34:52 -0500

    Change #27
    - Level 3 almost done

---
+++ Dream Theory/Assets/Scripts/Rotater.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+	public Transform playerTransform;
+
+	public float yspeed;
+
+	public float xspeed;
+
+	public float zspeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		playerTransform.Rotate(xspeed, yspeed, zspeed);
+	}
+}
